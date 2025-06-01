3201152d15634fea3b912df34ef894d5b56d9a2c
sherif rabiw karim <rabiwkarim55@gmail.com>
2025-06-01 22:30:58 +0000

    added new file

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+let firstStatement = "C is fun";
+let secondStatement = "Python is cool";
+let thirdStatement = "JavaScript is amazing";
+
+console.log(firstStatement);
+console.log(secondStatement);
+console.log(thirdStatement);
